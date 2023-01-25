fcd8776d2d8f6f6184626ebf30f02fa5d3b0c796
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2023-01-24 23:49:21 -0800

    Set up basic api architecture for bot database

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+import botRouter from './routes/bots';
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (_req, res) => {
 	res.json('Welcome to BruinBot API!');
 });
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('Successfully connected to mongoose instance!');
+	})
+	.catch((err) => {
+		console.log('Failed to connect to mongoose instance');
+		console.log('Error:\n', err);
+	});
+
+app.use(botRouter);
 
 export { app };
